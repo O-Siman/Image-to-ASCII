7feacd6d3846b82cd50e5acfff60bfee800f00d8
O-Siman <72103629+O-Siman@users.noreply.github.com>
2021-11-16 12:36:42 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,101 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = selectImage();
+        if (file == null) {
+            System.err.println("No file selected");
+            System.exit(1);
+            return;
+        }
+
+        System.out.println("Enter a width and height");
+        String widthString = JOptionPane.showInputDialog("Width");
+        String heightString = JOptionPane.showInputDialog("Height");
+
+        int width = -1, height = -1;
+        if (!widthString.equals("")) {
+            width = Integer.parseInt(widthString);
+            height = Integer.parseInt(heightString);
+        }
+
+        try {
+            asciiImage(file, width, height);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static File selectImage() {
+        // Select a temporary image with a file picker and return the selected file
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile();
+        }
+        return null;
+    }
+
+    public static void asciiImage(File file, int width, int height) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+        if (width != -1 && height != -1) {
+            image = resizeImage(width, height, image);
+        }
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                // Retrieve contents of a pixel
+                int pixel = image.getRGB(x,y);
+                // Create a Color object from pixel value
+                Color color = new Color(pixel, true);
+                //Retrieve the R G B values
+                int red = color.getRed();
+                int green = color.getGreen();
+                int blue = color.getBlue();
+                // Calculate the average of the R G B values
+                int average = (red + green + blue) / 3;
+
+                // Output
+                System.out.print("\033[7m");
+                if (x == 0)
+                    System.out.println();
+                if (average > 230)
+                    System.out.print(".");
+                else if (average > 200)
+                    System.out.print("'");
+                else if (average > 180)
+                    System.out.print("\"");
+                else if (average > 150)
+                    System.out.print(":");
+                else if (average > 120)
+                    System.out.print("*");
+                else if (average > 80)
+                    System.out.print("+");
+                else if (average > 50)
+                    System.out.print("%");
+                else if (average >= 20)
+                    System.out.print("#");
+                else if (average >= 0)
+                    System.out.print("@");
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static BufferedImage resizeImage(int width, int height, BufferedImage image) {
+        Image tmp = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = newImage.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return newImage;
+    }
+}
